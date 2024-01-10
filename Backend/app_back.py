846fa217826e4b3ff0a6a19b0485cd45ff37e659
Diego Peña <diegopp1995@gmail.com>
2024-01-10 18:48:31 +0100

    User Interface

---
+++ Backend/app_back.py
@@ -0,0 +1,26 @@
+# Script for the backend of the application.
+from flask import Flask, render_template, request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+# Esta ruta define la página principal de la aplicación.
+# Cuando los usuarios acceden a la ruta '/', la función 'index()' se ejecuta y muestra la plantilla 'index.html'.
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Esta ruta recibe solicitudes POST a la ruta '/send-location/<location_id>'.
+# La función 'send_location(location_id)' se ejecuta cuando se recibe una solicitud POST en esta ruta.
+# La variable 'location_id' se extrae de la URL y se pasa como argumento a la función.
+@app.route('/send-location/<location_id>', methods=['POST'])
+def send_location(location_id):
+    # Ejecutar el script del productor de Kafka con la ubicación proporcionada
+    subprocess.run(['python', 'kafka_producer_script.py', location_id])
+    # Responder con un JSON indicando que la operación fue exitosa.
+    return jsonify({'status': 'success'})
+
+# Esta condición verifica si el script está siendo ejecutado directamente y no importado como un módulo.
+if __name__ == '__main__':
+    # Iniciar la aplicación Flask en modo de depuración.
+    app.run(debug=True)
