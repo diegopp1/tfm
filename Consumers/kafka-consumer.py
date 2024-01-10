473cdf74fc1f6b8d7e01d8c87b94bd1fe90a5b9a
Diego Peña <diegopp1995@gmail.com>
2024-01-10 19:44:50 +0100

    Kafka-Consumer v2

--- Consumers/kafka-consumer.py
+++ Consumers/kafka-consumer.py
@@ -15,29 +15,34 @@ consumer = Consumer(conf)
 # Suscribirse a un tema (reemplaza 'my_topic' con el nombre real del tema)
 consumer.subscribe([topic])
 
-while True:
-    # Esperar mensajes
-    msg = consumer.poll(1.0)
+try:
+    while True:
+        # Esperar mensajes
+        msg = consumer.poll(1.0)
 
-    if msg is None:
-        continue
-    if msg.error():
-        if msg.error().code() == KafkaError._PARTITION_EOF:
-            # No se encontraron nuevos mensajes
+        if msg is None:
             continue
-        else:
-            print(msg.error())
-            break
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                # No se encontraron nuevos mensajes
+                continue
+            else:
+                print(msg.error())
+                break
 
-    # Decodificar y procesar el mensaje JSON
-    try:
-        data = json.loads(msg.value().decode('utf-8'))
+        # Decodificar y procesar el mensaje JSON
+        try:
+            data = json.loads(msg.value().decode('utf-8'))
 
-        # Aquí puedes realizar operaciones con los datos, como enviarlos a una base de datos o procesarlos de alguna otra manera
-        print("Datos recibidos:", data)
+            # Aquí puedes realizar operaciones con los datos, como enviarlos a una base de datos o procesarlos de alguna otra manera
+            print("Datos recibidos:", data)
 
-    except Exception as e:
-        print(f"Error al procesar mensaje: {e}")
+        except Exception as e:
+            print(f"Error al procesar mensaje: {e}")
 
-# Cerrar el consumidor
-consumer.close()
+except KeyboardInterrupt:
+    pass  # Manejar la interrupción del teclado (Ctrl+C)
+
+finally:
+    # Cerrar el consumidor
+    consumer.close()
