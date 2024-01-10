2cf70eb2a6adc5eece6a36ee3584310a1c892039
Diego Peña <diegopp1995@gmail.com>
2024-01-10 18:22:00 +0100

    Kafka producer v2

--- Producers/kafka-producer.py
+++ Producers/kafka-producer.py
@@ -14,15 +14,15 @@ producer = Producer(conf)
 # API Key de OpenAQ (reemplaza 'your-openaq-api-key' con tu clave real)
 openaq_api_key = 'your-openaq-api-key'
 
-# URL de la API de OpenAQ
-openaq_url = "https://api.openaq.org/v2/locations/"
+# URL base de la API de OpenAQ
+openaq_base_url = "https://api.openaq.org/v2/locations/"
 
-# Lista de regiones geográficas
-geographic_regions = ['europa', 'asia', 'africa', 'oceania']
+# Nombre del tema de Kafka (reemplaza 'my_topic' con el nombre real del tema)
+kafka_topic = 'my_topic'
 
-def fetch_openaq_data(region):
-    # Realizar la solicitud a la API de OpenAQ con la clave API y la región específica
-    response = requests.get(openaq_url + region, headers={"X-API-Key": openaq_api_key})
+def fetch_openaq_data(location_id):
+    # Realizar la solicitud a la API de OpenAQ con la clave API y el ID de la ubicación específica
+    response = requests.get(f"{openaq_base_url}{location_id}", headers={"X-API-Key": openaq_api_key})
     return response.json()
 
 def delivery_report(err, msg):
@@ -33,13 +33,13 @@ def delivery_report(err, msg):
 
 while True:
     try:
-        # Iterar sobre las regiones y obtener datos de OpenAQ para cada región
-        for region in geographic_regions:
-            openaq_data = fetch_openaq_data(region)
+        # Obtener datos de OpenAQ para la ubicación específica
+        # En este ejemplo, la ubicación se deja como un parámetro configurable (puede ser seleccionado desde el front end)
+        location_id = input("Introduce el ID de la ubicación de OpenAQ (por ejemplo, 2178): ")
+        openaq_data = fetch_openaq_data(location_id)
 
-            # Enviar datos al tema de Kafka específico para la región
-            kafka_topic = f'openaq_{region}'
-            producer.produce(kafka_topic, key=None, value=json.dumps(openaq_data).encode('utf-8'), callback=delivery_report)
+        # Enviar datos al tema de Kafka
+        producer.produce(kafka_topic, key=None, value=json.dumps(openaq_data).encode('utf-8'), callback=delivery_report)
 
         # Esperar un breve período de tiempo antes de obtener nuevos datos
         time.sleep(60)  # Espera 60 segundos (puedes ajustar este valor)
@@ -49,3 +49,4 @@ while True:
 
 # Esperar a que todos los mensajes se entreguen antes de cerrar el productor
 producer.flush()
+
