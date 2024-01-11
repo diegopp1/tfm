84e48a35490420f4f5fc7065350bbc1b4f4bcb54
Diego Peña <diegopp1995@gmail.com>
2024-01-11 17:42:39 +0100

    Backend_WS

--- Backend/app_back.py
+++ Backend/app_back.py
@@ -1,26 +1,54 @@
-# Script for the backend of the application.
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify
+from flask_socketio import SocketIO
 import subprocess
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-# Esta ruta define la página principal de la aplicación.
-# Cuando los usuarios acceden a la ruta '/', la función 'index()' se ejecuta y muestra la plantilla 'index.html'.
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Esta ruta recibe solicitudes POST a la ruta '/send-location/<location_id>'.
-# La función 'send_location(location_id)' se ejecuta cuando se recibe una solicitud POST en esta ruta.
-# La variable 'location_id' se extrae de la URL y se pasa como argumento a la función.
 @app.route('/send-location/<location_id>', methods=['POST'])
 def send_location(location_id):
-    # Ejecutar el script del productor de Kafka con la ubicación proporcionada
     subprocess.run(['python', 'Producer (OpenAQ)/kafka-producer.py', location_id], cwd='..')
-    # Responder con un JSON indicando que la operación fue exitosa.
     return jsonify({'status': 'success'})
 
-# Esta condición verifica si el script está siendo ejecutado directamente y no importado como un módulo.
+def kafka_consumer():
+    from confluent_kafka import Consumer, KafkaError, KafkaException
+
+    conf = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'my_consumer_group',
+        'auto.offset.reset': 'earliest'
+    }
+    topic = 'json-topic'
+    consumer = Consumer(conf)
+
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    raise KafkaException(msg.error())
+
+            try:
+                data = json.loads(msg.value().decode('utf-8'))
+                print("Datos recibidos:", data)
+                socketio.emit('air_quality_data', data)  # Emitir datos a través de WebSocket
+            except Exception as e:
+                print(f"Error al procesar mensaje: {e}")
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
+
 if __name__ == '__main__':
-    # Iniciar la aplicación Flask en modo de depuración.
-    app.run(debug=True)
+    socketio.start_background_task(target=kafka_consumer)
+    socketio.run(app, debug=True)
