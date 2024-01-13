ab0706058f217e0a76f38d9bdc22f7e433821375
Diego Peña <diegopp1995@gmail.com>
2024-01-13 13:32:10 +0100

    ALL_13012023

--- Backend/app_back_v2.py
+++ Backend/app_back_v2.py
@@ -3,10 +3,10 @@ from flask_socketio import SocketIO, emit
 from confluent_kafka import Consumer, KafkaError
 import json
 import logging
-import time
 
-app = Flask(__name__)
-socketio = SocketIO(app, threaded=True)
+
+app = Flask(__name__) # Inicializar la aplicación Flask
+socketio = SocketIO(app, threaded=True) # Inicializar SocketIO
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('consumer')
@@ -18,28 +18,13 @@ kafka_conf = {
 }
 kafka_topic = 'datos'
 
-consumer = Consumer(kafka_conf)
-consumer.subscribe([kafka_topic])
+consumer = Consumer(kafka_conf) # Crear un consumidor
+consumer.subscribe([kafka_topic]) # Suscribirse a un tema
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
-
-@app.route('/start-stream')
-def start_stream():
-    time.sleep(10)
-    msg = consumer.poll(1.0)
-    print(type(msg))
-    if msg is not None:
-        try:
-            data = json.loads(msg.value().decode('utf-8'))
-            print(data)
-            return jsonify(data)
-        except Exception as e:
-            logger.error(f"Error al procesar mensaje: {e}")
-    else:
-        return jsonify({'error': 'No hay datos'})
-
+    return render_template('index2.html') # Renderizar la plantilla index.html
+# def consume_message(): Función que consume mensajes de kafka
 def consume_message():
     msg = consumer.poll(1.0)
     if msg is not None and not msg.error():
@@ -50,7 +35,7 @@ def consume_message():
         except Exception as e:
             logger.error(f"Error al procesar mensaje: {e}")
     return None
-
+# def background_thread(): Función que envía mensajes a través de SocketIO
 def background_thread():
     while True:
         data = consume_message()
@@ -67,6 +52,3 @@ if __name__ == '__main__':
     socketio.start_background_task(target=background_thread)
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True, use_reloader=False)
 
-
-# Mirar con DASH
-# https://dash.plotly.com/live-updates
