290d47d1c5f00446fe59ad813703281b38fd76a8
Diego Peña <diegopp1995@gmail.com>
2024-01-29 16:13:09 +0100

    ALL_v4_29012024

--- Backend/app_back_v6_loc.py
+++ Backend/app_back_v6_loc.py
@@ -122,6 +122,7 @@ def get_graph_data():
     # Si el eje X es 'country' y el eje Y es pm10, pm25 o um100
     if x_axis_field == 'country' and any(y_axis_field.endswith(f'({param})') for param in y_parameters):
         averages_by_country = calculate_average_by_country(mongo_locations_collection)
+        print(averages_by_country)
         return jsonify(averages_by_country)
 
     # Si el eje X es 'name', proceder con la lógica original
